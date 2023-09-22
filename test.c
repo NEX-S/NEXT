74f8c089d6a08b0eeeca217d7935b0d6a661704e
NEX <veperx@icloud.com>
2023-09-22 19:13:14 +0800

    update

---
+++ test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    printf("Hello World 0");
+    printf("Hello World 1");
+    printf("Hello World 2");
+    printf("Hello World 3");
+    printf("Hello World 4");
+    printf("Hello World 5");
+    printf("Hello World 6");
+    printf("Hello World 7");
+    printf("Hello World 8");
+    printf("Hello World 9");
+    printf("Hello World 10");
+    printf("Hello World 11");
+    printf("Hello World 12");
+    printf("Hello World 13");
+    printf("Hello World 14");
+    printf("Hello World 15");
+    printf("Hello World 16");
+    printf("Hello World 17");
+    printf("Hello World 18");
+    return 0;
+}
