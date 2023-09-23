359207e16b1f948fdda2e238a0d8544c8cb30e3e
NEX <veperx@icloud.com>
2023-09-23 14:48:43 +0800

    update

--- lua/ui/fold.lua
+++ lua/ui/fold.lua
@@ -1,14 +1,18 @@
 local api = vim.api
 
-os.execute("mkdir -p ~/.cache/nvim/view")
+-- os.execute("mkdir -p ~/.cache/nvim/view")
 
 local fold_options = {
-    viewdir = "~/.cache/nvim/view",
-    foldenable = true,
-    foldcolumn = "1",
+    foldenable   = true,
+    -- foldmethod   = "indent",
     foldminlines = 4,
-    foldmethod = "indent",
-    foldtext = "v:lua.FOLD_TEXT()",
+    foldcolumn   = '1',
+    foldtext     = "v:lua.FOLD_TEXT()",
+    -- viewdir      = "~/.cache/nvim/view",
+
+    -- treesitter fold
+    foldmethod = "expr",
+    foldexpr = "nvim_treesitter#foldexpr()",
 }
 
 vim.opt.fillchars:append("fold: ,foldsep:╎,foldopen:~,foldclose:~")
@@ -19,7 +23,6 @@ end
 
 _G.FOLD_TEXT = function ()
     local s_row = vim.v.foldstart
-    local i = 0
     local s_str = api.nvim_buf_get_lines(0, s_row - 1, s_row, false)[1]
 
     return s_str .. ' '
@@ -27,5 +30,11 @@ end
 
 api.nvim_set_keymap('n', ' ', "za", { noremap = true })
 
-api.nvim_create_autocmd("BufReadPost", { command = "silent! loadview" })
-api.nvim_create_autocmd("BufWinLeave", { command = "silent! mkview" })
+api.nvim_create_autocmd("BufReadPost", {
+    callback = function ()
+        api.nvim_input("zR")
+    end
+})
+
+-- api.nvim_create_autocmd("BufReadPost", { command = "loadview" })
+-- api.nvim_create_autocmd("BufLeave", { command = "silent! mkview" })
