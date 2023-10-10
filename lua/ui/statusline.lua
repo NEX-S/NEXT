bba314e51877432341a45b883cf4d09259830769
NEX <veperx@icloud.com>
2023-10-10 13:28:38 +0800

    update

--- lua/ui/statusline.lua
+++ lua/ui/statusline.lua
@@ -15,11 +15,17 @@ local api = vim.api
 --     return ft_icon[vim.bo.ft] or ''
 -- end
 
+_G.STATUSLINE_FILE_PATH = function ()
+    local file_path = api.nvim_buf_get_name(0)
+    return file_path:match("^/") and file_path or ""
+end
+
 local L_FT   = "%#StatusLineFT# %Y %#StatusLineFTSep#"
 -- local L_GIT  = "%#StatusLineGitSepL#%#StatusLineGit#  %{% v:lua.STATUSLINE_GIT() %} %#StatusLineGitSepR#"
 local L_GIT  = "%#StatusLineGitSepL#%#StatusLineGit#  %{ luaeval('_G.GIT_BRANCH') } %#StatusLineGitSepR#"
 -- local L_GIT  = "%#StatusLineGitSepL#%#StatusLineGit# GIT %#StatusLineGitSepR#"
-local L_PATH  = "%#StatusLinePathSepL#%#StatusLinePATH#%F %{% &modified ? '%#StatusLineMod# ' : '' %}%#StatusLinePathSepR#"
+-- local L_PATH  = "%#StatusLinePathSepL#%#StatusLinePATH#%F %{% &modified ? '%#StatusLineMod# ' : '' %}%#StatusLinePathSepR#"
+local L_PATH  = "%#StatusLinePathSepL#%#StatusLinePATH#%{ v:lua.STATUSLINE_FILE_PATH() } %{% &modified ? '%#StatusLineMod# ' : '' %}%#StatusLinePathSepR#"
 
 local R_LINE = "%#StatusLineRowSep#%#StatusLineRow# %l / %L "
 local R_CHAR = "%#StatusLineCharSepL#%#StatusLineChar# %c   %B %#StatusLineCharSepR#"
